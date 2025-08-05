64413aa1693883c201ace5e188f3e3dc1f9a3791
hetpatel2004 <het.patel.010420047@gmail.com>
2025-08-05 20:21:23 +0530

    05-Aug

---
+++ list.py
@@ -0,0 +1,34 @@
+het =["patel",21, 2004, "male", 56.7]
+print(het)
+# minus goes bottam to topp 
+print(het[-1])
+print(type(het[1]))
+print(type(het[2]))
+print(type(het[3]))
+print(type(het[4]))
+print(type(het[0]))
+print(het*5)
+# print(type(het[-4]))
+
+
+
+# a = [10, 20, 30, 40, 50]
+# print(a)
+
+# b =["Banana", "Apple", "Orange", "Grape", "Kiwi"]
+# print(b)
+
+# c = [5,10,15.20,'Python', True]
+# print(type(c[-2]))
+
+
+
+# a = ['jaguar'] * 5
+# print(a)
+
+# b = [5*5, 102, 153, 204, 25*5]
+# print(b)
+
+# # from a tuple to a list
+# c = [5,10,15.20,'Python', True]
+# print(c)
