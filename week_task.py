11d6a0283ff655986e7b16c48876f4e8a4ce41d2
hetpatel2004 <het.patel.010420047@gmail.com>
2025-08-24 01:11:31 +0530

    24-Aug

--- week_task.py
+++ week_task.py
@@ -1,4 +1,4 @@
-# 1. Write a Python program to swap two variables without using a third variable.
+# 1. Write a Python program to swap two variables without using a third variable.------------------------------------------
 # a = 10
 # b = 20
 
@@ -8,14 +8,14 @@
 # print("a = ",a ,"b = ", b) 
 
 
-# 2.Create a program that takes your name and age as input and prints them in a sentence.
+# 2.Create a program that takes your name and age as input and prints them in a sentence.------------------------------------------
 
 # name = input("Enter your Name :")
 # age = int(input("Enter your age :"))
 
 # print("Hello ",name, "you age",age) 
 
-# 3.Write a program to calculate the area of a rectangle using user input for length and width.
+# 3.Write a program to calculate the area of a rectangle using user input for length and width.------------------------------------------
 
 # length = int(input("Enter length :"))
 # width = int(input("Enter length :"))
@@ -24,7 +24,7 @@
 
 # print("Area of Rectangle", rec)
 
-# 4.Store your favorite movie name, release year, and rating in variables and print them in a formatted string.
+# 4.Store your favorite movie name, release year, and rating in variables and print them in a formatted string.------------------------------------------
 
 # movie_name = input("Enter Your Favorite movie Name :")
 # release_year = int(input("Enter Your Favorite movie year :"))
@@ -33,14 +33,14 @@
 # print( "Your Favorite movie is ",movie_name,"in",release_year," it was reasised you rated it",Rating,)
 
 
-# 5.Write a program to convert temperature from Celsius to Fahrenheit.
+# 5.Write a program to convert temperature from Celsius to Fahrenheit.------------------------------------------
 
 # cal = int(input("enter cal"))
 # far = cal * (9 / 5)
 # print(cal)
 # print(far)
 
-# 6.Take input from the user and check its data type using type().
+# 6.Take input from the user and check its data type using type().------------------------------------------
 
 # var = input("Enter string  value")
 # print(type(var))
@@ -51,7 +51,7 @@
 # var3 = bool(input("Enter any bool"))
 # print(type(var3))
 
-# 7.Write a program that accepts two integers and prints their sum, difference, product, and quotient.
+# 7.Write a program that accepts two integers and prints their sum, difference, product, and quotient.------------------------------------------
 
 # num1= int(input("Enter a number one for Sum "))
 # num2= int(input("Enter a number second for Sum "))
@@ -63,6 +63,48 @@
 # print(num1 * num2)
 # print(num1 / num2)
 
-# 8.Create a program that checks whether a given number is even or odd.
-num1 = int(input("Enter the number to find reather its even or odd :"))
 
+# 8.Create a program that checks whether a given number is even or odd.------------------------------------------
+
+# num1 = int(input("Enter the number to find reather its even or odd :"))
+
+# even = (num1 % 2) 
+# even2 = (even == 0)
+
+# print("False means it's 'odd' number and true if the given number is 'even'")
+# print("your output is :",even2)
+
+
+# 9.Write a program that checks whether the entered number is positive, negative, or zero.------------------------------------------
+
+# num = int(input("Enter a number i will check it is positive nagitive or zero :"))
+
+# if num > 0 :
+#     print("positive")
+# elif num < 0 :
+#     print("negitive")
+# else:
+#     print("its Zero")
+
+
+# 10.Concatenate two strings and print the result with proper formatting.------------------------------------------
+
+# str1 = input("Enter your name :")
+# str2 = input("Enter your last name :")
+
+# total = str1 + str2
+
+# print("Your full name ",total)
+
+
+# 11.Write a program that calculates the square and cube of a given number using exponent operator **.------------------------------------------
+
+# 12.Demonstrate the use of // (floor division) and % (modulus) operators.------------------------------------------
+# 13.Write a program that takes three numbers and prints the largest one using comparison operators.------------------------------------------
+# 14.Use logical operators (and, or, not) in a program to check eligibility for voting (age ≥ 18 and citizenship = True).------------------------------------------
+# 15.Write a program to check if a number is divisible by both 3 and 5.------------------------------------------
+# 16.Show the difference between == and is operators with examples.------------------------------------------
+# 17.Write a program to calculate simple interest (SI = PRT/100).------------------------------------------
+# 18.Demonstrate assignment operators (+=, -=, *=, /=, etc.) on a variable.------------------------------------------
+# 19.Write a program to calculate BMI (Body Mass Index).------------------------------------------
+# 20.Create a program to find the remainder when one number is divided by another.------------------------------------------
