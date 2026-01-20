3b3c30043ec99eec225239f2b2edfeb2795d6560
hetpatel2004 <het.patel.010420047@gmail.com>
2026-01-20 12:39:45 +0530

    20-jan-2026

---
+++ Sou/basic.py
@@ -0,0 +1,21 @@
+# print("het")
+# a = int(input("enter a value "))
+# b = int(input("enter a value "))
+
+a= 10 
+b= 1
+c = 8
+# print(c)
+
+# print("value of a:-",a,"value of b:-",b,"sum of a and b is :-",c)
+# print(f"value of a:-{a}value of b:-{b}sum of a and b is :-{c}")
+# print(type(a))
+# a = 5.5
+# print(type(a))
+# a = "abc"
+# print(type(a))
+
+# print(type(b))
+
+print(a>b and a>c)
+print(b>a and b>c)
---
+++ Sou/test.py
@@ -0,0 +1,40 @@
+# # marks = int(input("entar your marks"))
+# # if (marks >= 90):
+# #     print("Grade:-A")
+# # elif (marks >= 80):
+# #     print("Grade:-b")
+# # elif (marks >= 70):
+# #     print("Grade:-c")
+# # elif (marks >= 60):
+# #     print("Grade:-D")
+# # else: 
+# #     print("Grade:- fail")
+
+
+
+# price = 15000
+# roi= 7
+# year= 12
+
+# print (price*roi*year /100)
+
+
+# basic = 20000
+# har = basic*0.2
+# da = basic*0.1
+
+# salary = basic + har + da
+# print(salary)
+
+
+
+# num = int(input("enter the number"))
+# if(num %2 == 0):
+#     print("Even")
+# else:
+#     print("ODD")
+
+# a= 320
+# a += 10
+# print(a)
+
